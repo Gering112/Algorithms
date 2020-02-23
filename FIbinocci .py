f0b26ae46041f20f5f4ca286f5e974bf21549cb5
Gering112 <45523369+Gering112@users.noreply.github.com>
2020-02-23 13:24:30 -0500

    Update FIbinocci .py

--- FIbinocci .py
+++ FIbinocci .py
@@ -1,9 +1,9 @@
 def fib (a):
 
-    # return 0 if input is 0
+    # return 1 if input is 0
     if a == 1:
         return 0
-    # return 1 if input is 1
+    # return 2 if input is 1
     elif a == 2:
         return 1
     else:
